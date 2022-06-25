181176db8de7b35977457bf453afe49019a2e653
PaSeRouS <paser96@mail.ru>
2022-06-25 22:42:10 +0300

    optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments.count(),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -31,22 +45,24 @@ def serialize_tag(tag):
 
 def index(request):
     most_popular_posts = Post.objects.annotate(
-        count_likes=Count('likes')
-    ).order_by('-count_likes')[:5].prefetch_related('author')
+        likes_count=Count('likes')
+    ).order_by('-likes_count')[:5].prefetch_related('author', 'comments')
 
     most_fresh_posts = Post.objects.order_by(
         '-published_at'
-    )[:5].prefetch_related('author')
+    )[:5].prefetch_related('author', 'comments')
 
     most_popular_tags = Tag.objects.annotate(
-        count_tags=Count('posts')
+        count_tags=Count('posts'),
     ).order_by('-count_tags')[:5]
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(
+            post
+        ) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
